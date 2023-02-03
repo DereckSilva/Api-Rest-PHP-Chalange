c0cad6ed586931b336da83217401add7788d9387
DereckSilva <viniciusdereck39@gmail.com>
2023-02-02 22:25:38 -0300

    parametrizando schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Jobs\DownloadFiles;
+use App\Jobs\GetZipFile;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -15,7 +17,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('baixa:arquivo')->cron('* * * * *');
+        $schedule->job(DownloadFiles::dispatch('https://challenges.coode.sh/food/data/json/index.txt',
+        '/openFood/products.txt'));
+
+        $schedule->job(GetZipFile::dispatch('https://challenges.coode.sh/food/data/json/',
+        '/var/www/html/storage/app/openFood/products.txt'));
        // $schedule->command('exclui:arquivo')->cron('* * * * *');
     }
 
